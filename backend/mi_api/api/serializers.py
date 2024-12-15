0dd266521ad8d5075e3cdabf695ba39994b7cb88
narmijo-tj <toffee.jaguargmail.com>
2024-12-14 21:51:32 -0300

    Se agregan validaciones usuario duplicado. Falta comprobar el no acciones crud usuarios no admin

--- backend/mi_api/api/serializers.py
+++ backend/mi_api/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Categoria, Evento, Participante
+from rest_framework.exceptions import ValidationError
 
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,6 +16,11 @@ class ParticipanteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Participante
         fields = ['id', 'nombre', 'correo', 'evento']
+        extra_kwargs = {
+            'nombre': {'error_messages': {'blank': "El nombre no puede estar vacío."}},
+            'correo': {'error_messages': {'blank': "El correo no puede estar vacío."}},
+            'evento': {'error_messages': {'null': "Debe especificar un evento válido."}}
+        }
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
@@ -25,3 +31,13 @@ class ParticipanteSerializer(serializers.ModelSerializer):
             representation.pop('correo', None)
 
         return representation
+    
+    def validate(self, data):
+        # Validar que un usuario no se registre dos veces en el mismo evento
+        evento = data.get('evento')
+        correo = data.get('correo')
+
+        if Participante.objects.filter(evento=evento, correo=correo).exists():
+            raise ValidationError("Este usuario ya está registrado en el evento.")
+        
+        return data
